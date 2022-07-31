0f461b344c5d9d55bc98b2f43f1b944464c0145d
nxjosephofficial <nxjosephofficial@protonmail.com>
2022-07-31 11:46:33 +0000

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :players
   get 'home/index'
+  get 'players/edit'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
